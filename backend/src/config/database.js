23bc15184472c82dc21217345e5f8e3a0bd24524
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2023-11-28 19:38:17 -0300

    * Atualização do Mongoose. * Inclusão da rota de sumário.

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,4 +1,4 @@
 const { mongo } = require('mongoose');
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-module.exports = mongoose.connect('mongodb://localhost/mymoney');
+module.exports = mongoose.connect('mongodb://localhost/mymoney', { useNewUrlParser: true});
