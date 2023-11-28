1dfce5433a0ef5d7a11df6d1d3b8ce75832be308
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2023-11-28 19:13:31 -0300

    Implementação das rotas. Implementação do contador.

---
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -0,0 +1,26 @@
+const BillingCycle = require('./billingCycle');
+
+BillingCycle.methods(['get', 'post', 'put', 'delete']);
+BillingCycle.updateOptions({ new: true, runValidators: true });
+
+BillingCycle.route('get', (req, res, next) => {
+    BillingCycle.find({}, (err, docs) => {
+        if (!err) {
+            res.json(docs);
+        } else {
+            res.status(500).json({ errors: [err] })
+        }
+    })
+});
+
+BillingCycle.route('count', (req, res, next) => {
+    BillingCycle.count((error, value) => {
+        if (error) {
+            res.status(500).json({ errors: [error]});
+        } else {
+            res.json({ value });
+        }
+    });
+});
+
+module.exports = BillingCycle;
--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,6 +1,8 @@
 const port = 3003;
 const bodyParser = require('body-parser');
 const express = require('express');
+const { mongo } = require('mongoose');
+const mongoose = require('mongoose');
 const server = express();
 
 server.use(bodyParser.urlencoded({ extended: true }));
@@ -8,3 +10,10 @@ server.use(bodyParser.json());
 server.listen(port, function () {
     console.log(`BACKEND está rodando na porta ${port}`);
 });
+
+module.exports = server;
+
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório.";
+mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite mínimo '{MIN}'.";
+mongoose.Error.messages.Number.max = "O '{VALUE}' informado é maior que o limite máximo '{MAX}'.";
+mongoose.Error.messages.String.enum = "{VALUE} não é válido para o atributo '{PATH}'.";
