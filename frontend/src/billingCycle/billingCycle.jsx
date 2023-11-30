8b373f7360e5d5b5009897187ed694fbb6188344
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2023-11-30 13:50:13 -0300

    Criação do componente TabHeader e implementação das abas.

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -4,6 +4,7 @@ import ContentHeader from '../common/template/contentHeader';
 import Content from '../common/template/content';
 import Tabs from '../common/tab/tabs';
 import TabsHeader from '../common/tab/tabsHeader';
+import TabHeader from '../common/tab/tabHeader';
 import TabsContent from '../common/tab/tabsContent';
 
 
@@ -18,8 +19,31 @@ class BillingCycle extends Component {
                 />
                 <Content>
                     <Tabs>
-                        <TabsHeader></TabsHeader>
-                        <TabsContent></TabsContent>
+                        <TabsHeader>
+                            <TabHeader
+                                label="Listar"
+                                icon="bars"
+                                target="tabList"
+                            />
+                            <TabHeader
+                                label="Incluir"
+                                icon="plus"
+                                target="tabCreate"
+                            />
+                            <TabHeader
+                                label="Alterar"
+                                icon="pencil"
+                                target="tabUpdate"
+                            />
+                            <TabHeader
+                                label="Excluir"
+                                icon="trash-o"
+                                target="tabDelete"
+                            />
+                        </TabsHeader>
+                        <TabsContent>
+
+                        </TabsContent>
                     </Tabs>
                 </Content>
             </div>
