bc482789fe0857d5e3dace867b8ae7247839c2cb
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2023-11-30 10:00:46 -0300

    Criação do sidebar

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 import '../common/template/dependences';
 
 import Header from '../common/template/header';
+import SideBar from '../common/template/sideBar';
 
 export default props => (
     <div className='wrapper'>
         <Header />
+        <SideBar />
     </div>
 );
