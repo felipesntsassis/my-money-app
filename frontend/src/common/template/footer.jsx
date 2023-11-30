c308015310f440b75e861c667fd3b186f7d20459
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2023-11-30 10:19:20 -0300

    Criação do componente de rodapé.

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default props => (
+    <footer className="main-footer">
+        <strong>
+            Copyright &copy; 2023
+            <a href="https://dev.works.com.br" target="_blank">
+             Dev Works
+            </a>.
+        </strong>
+    </footer>
+);
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -4,10 +4,15 @@ import '../common/template/dependences';
 
 import Header from '../common/template/header';
 import SideBar from '../common/template/sideBar';
+import Footer from '../common/template/footer';
 
 export default props => (
     <div className='wrapper'>
         <Header />
         <SideBar />
+        <div className="content-wrapper">
+            <h1>Conteúdo</h1>
+        </div>
+        <Footer />
     </div>
 );
