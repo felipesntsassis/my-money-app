c064ab20b992b3ca8b4a9893e5122614508b3162
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2023-12-06 07:12:17 -0300

    Integração da soma de pagamentos.

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -10,8 +10,18 @@ import Summary from './summary';
 import { init } from './billingCycleActions';
 class BillingCycleForm extends Component {
 
+    calculateSummary() {
+        const sum = (t, v) => t + v;
+
+        return {
+            sumOfCredits: this.props.credits.map(c => +c.value || 0).reduce(sum),
+            sumOfDebts: this.props.debts.map(d => +d.value || 0).reduce(sum)
+        };
+    }
+
     render() {
         const { handleSubmit, readOnly, credits, debts } = this.props;
+        const { sumOfCredits, sumOfDebts} = this.calculateSummary();
 
         return (
             <form role="form" onSubmit={handleSubmit}>
@@ -43,8 +53,8 @@ class BillingCycleForm extends Component {
                         type="number"
                     />
                     <Summary
-                        credit="1000"
-                        debt="100"
+                        credit={sumOfCredits}
+                        debt={sumOfDebts}
                     />
                     <ItemList
                         cols="12 6"
