d73de1ec63967d756d5310ffb5838e511fbb3ff1
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2023-12-16 11:42:43 -0300

    Criação do input para a tela de autenticação.

--- frontend/src/auth/authReducer.js
+++ frontend/src/auth/authReducer.js
@@ -1,7 +1,6 @@
 const userKey = '_mymoney_user';
 const INITIAL_STATE = {
-    // user: JSON.parse(localStorage.getItem(userKey)),
-    user: { name: 'Teste', email: 'teste@teste.com.br' },
+    user: JSON.parse(localStorage.getItem(userKey)),
     validToken: false
 };
 
