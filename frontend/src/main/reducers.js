fcfca6baa6c96507f60ffbd9a8188ee954076597
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2023-12-16 11:05:32 -0300

    Implementação dos recursos necessários para efetuar a autenticação.

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -2,11 +2,13 @@ import { reducer as toastrReducer } from 'react-redux-toastr';
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 
+import AuthReducer from '../auth/authReducer';
 import BillingCycleReducer from '../billingCycle/billingCycleReducer';
 import TabReducer from '../common/tab/tabReducer';
 import DashboardReducer from '../dashboard/dashboardReducer';
 
 const rootReducer = combineReducers({
+    auth: AuthReducer,
     billingCycle: BillingCycleReducer,
     dashboard: DashboardReducer,
     form: formReducer,
